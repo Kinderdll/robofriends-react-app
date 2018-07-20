6ef1d0107523c24cee14230339678598a8e9e8a8
Dellis <efthimis.dellis@peoplecert.org>
2018-07-20 16:54:03 +0300

    lets go

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import CardList from './CardList'
+import App from '../src/containers/App'
+import 'tachyons'
+import registerServiceWorker from './registerServiceWorker';
+//import {robots} from './robots'
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+registerServiceWorker();
